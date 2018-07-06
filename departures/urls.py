9330b8ea838419773d8f18dc3daecc73dd383d7a
Daniel Lee <newrealtopten@gmail.com>
2018-07-06 07:20:54 -0700

    add new endpoint to load fixtures from json

--- departures/urls.py
+++ departures/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
 
 from .views import DepartureView
+from departures import views
 
 urlpatterns = [
-    url('^', DepartureView.as_view())
+    url(r'^load-fixtures', views.load_fixtures, name="load_fixtures"),
+    url(r'^', DepartureView.as_view()),
+
 ]
--- departures/views.py
+++ departures/views.py
@@ -2,6 +2,8 @@ from rest_framework import generics
 from rest_framework import serializers
 
 from .models import Departure
+from django.http import HttpResponse
+
 
 class DepartureSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,3 +13,15 @@ class DepartureSerializer(serializers.ModelSerializer):
 class DepartureView(generics.ListAPIView):
     queryset = Departure.objects.all()
     serializer_class = DepartureSerializer
+
+import json
+
+def load_fixtures(request):
+    number_of_new_departure = 0;
+    with open('departures.json') as json_data:
+        departure_fixtures = json.load(json_data)
+        for departure in departure_fixtures:
+            if Departure.objects.filter(name=departure["name"], start_date=departure["start_date"], finish_date=departure["finish_date"], category=departure["category"]).count()==0:
+                Departure(name=departure["name"], start_date=departure["start_date"], finish_date=departure["finish_date"],category=departure["category"]).save()
+                number_of_new_departure += 1
+    return HttpResponse( "%d new departures successfully inserted" % (number_of_new_departure))
